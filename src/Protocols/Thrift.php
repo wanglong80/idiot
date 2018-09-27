2197fdbda0c0fd2a9c03c382be3cf0169adf0ab1
flowithwind <liushi5216@gmail.com>
2018-09-27 10:11:30 +0800

    add dubbo-thrift support

--- src/Protocols/Thrift.php
+++ src/Protocols/Thrift.php
@@ -8,6 +8,15 @@
  * For full copyright and license information, please see the LICENSE file
  * Redistributions of files must retain the above copyright notice.
  *
+ *
+	思路：
+	1、搞清楚dubbo的thrift加了哪些header，相比原生thrift
+	2、给予idiot升级，增加一个protocol，并且继承thrift的protocol，实现dubbo的额外封装逻辑
+
+
+
+
+ *
  * dubbo-thrift protocol implement 
  * <pre>
  * |<-                                  message header                                  ->|<- message body ->|
@@ -15,6 +24,13 @@
  * | magic (2 bytes)|message size (4 bytes)|head size(2 bytes)| version (1 byte) | header |   message body   |
  * +----------------+----------------------+------------------+---------------------------+------------------+
  * |<-                                               message size                                          ->|
+ * <p>
+ * <b>header fields in version 1</b>
+ * <ol>
+ *     <li>string - service name</li>
+ *     <li>long   - dubbo request id</li>  i64
+ * </ol>
+ * </p>
  * </pre>
  *
  * @author   Shi Liu  < liushi5216@gmail.com >
@@ -23,13 +39,21 @@
  
 namespace Idiot\Protocols;
 use Exception;
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Transport\TSocket;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Exception\TException;
 
 class Thrift extends AbstractProtocol
 {
     public function connect($host, $port, $path, $method, $args, $group, $version, $dubboVersion = self::DEFAULT_DUBBO_VERSION)
     {
-       //TODO  fix 
-       
+        //TODO  fix 
+      	$socket = new TSocket($host, $port);
+		$transport = new TBufferedTransport($socket, 1024, 1024);
+  		$protocol = new TBinaryProtocol($transport);
+  		
+
         return $data;
     }
 }
